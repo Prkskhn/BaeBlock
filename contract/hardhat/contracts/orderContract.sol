d31d35bf97e5f03bc5c177261745022cfa4219c3
Prkskhn <tjrgns7452@gmail.com>
2023-07-17 14:49:11 +0900

    npx hardhat

--- contract/hardhat/contracts/orderContract.sol
+++ contract/hardhat/contracts/orderContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./riderNft.sol";
 import "./storeNft.sol";
 
--- contract/hardhat/contracts/riderNft.sol
+++ contract/hardhat/contracts/riderNft.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.18;
 
-import "./node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "./node_modules/@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "./node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract delivery is ERC1155Supply,Ownable {
