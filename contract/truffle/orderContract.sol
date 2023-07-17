e44c660a19048196e4e40e205d81cc3b83e63515
Prkskhn <tjrgns7452@gmail.com>
2023-07-17 10:21:45 +0900

    settings

---
+++ contract/truffle/orderContract.sol
@@ -0,0 +1,224 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./riderNft.sol";
+import "./storeNft.sol";
+
+
+contract Payment is Ownable {
+// HS-rider 가져오기-----------------------------------------------------------------
+   
+    delivery riderContract;
+    store Store;
+
+    constructor(address _riderContractAddr,address _storeContractAddr){
+        riderContract= delivery(payable (_riderContractAddr));
+        Store= store(payable (_storeContractAddr));
+    }
+
+    uint platformFee = 2;   //플랫폼 수수료
+    event Result(string message);
+
+// 변수선언 -----------------------------------------------------------------------
+    
+    //주문 번호
+    uint public orderID;       
+    
+    //주문 구조체
+    struct Order {
+        address cWallet;    //고객 지갑
+        address sWallet;    //가게 지갑
+        address rWallet;    //라이더 지갑
+        uint foodPrice;     //음식 가격
+        uint deliveryFee;   //거리에 따라 지정되는 배달비 (고객 + 가게)
+        uint deliveryTip;   //배달팁
+        orderState status;  //주문 상태
+    }
+
+    //주문상태
+    enum orderState{
+        order,
+        store_accept,
+        store_decline,
+        store_cookFinish,
+        rider_accept,
+        rider_inDelivery,
+        rider_deliveryComplete,
+        done
+    }
+    
+
+    //주문번호로 주문 조회 (orderID=>Order)
+    mapping(uint => Order) searchOrder; 
+
+    //역할
+    enum Role{
+        store,
+        rider
+    }
+
+    //회원 구조체
+    struct Member {
+        address Wallet;
+        Role role;
+    }
+
+    //회원 목록
+    mapping(address => Member) members;
+
+// 정보등록 -----------------------------------------------------------------------
+
+    //회원가입
+    function Register(uint _role) public {
+        if(_role==1){
+            members[msg.sender]= Member(msg.sender, Role.store);
+        }else if(_role==2){
+            members[msg.sender]= Member(msg.sender, Role.rider);
+        }
+    }
+
+    //확인
+    function returnRegister()public view returns(address,Role){
+        return(members[msg.sender].Wallet, members[msg.sender].role);
+    }
+
+// 결제 ------------------------------------------------------------------------------
+    //고객이 주문하고 돈을 보냄
+    function ordering(address _sWallet, uint _foodPrice, uint _deliveryFee, uint _deliveryTip) public payable {
+        require(msg.value == (_foodPrice + _deliveryFee + _deliveryTip));           //잔고 확인
+        require(members[_sWallet].role == Role.store);                              //사용가능한 가게인지 확인
+        searchOrder[orderID] = Order(msg.sender, _sWallet, address(0) ,_foodPrice, _deliveryFee, _deliveryTip, orderState.order);
+        orderID++;
+    }
+    
+    function returnOrderID()public view returns(uint){
+        return orderID-1;
+    }
+
+    //확인
+    function returnOrder(uint _orderID)public view returns(Order memory){
+        return searchOrder[_orderID];
+    }
+
+    //가게 : 주문 수락시 배달비 지불 / 거절시 지불 안됨
+    function storeAccept_Decline(uint _orderID, bool _bool) public payable {
+        require(msg.sender == searchOrder[_orderID].sWallet, "You can't access this order.");           //가게 주인확인
+        require(searchOrder[_orderID].status == orderState.order, "This order is not available.");      //주문 상태 확인
+        if(_bool==true){
+            require(msg.value == (searchOrder[_orderID].deliveryFee), "You should pay DeliveryFee");  //배달비 확인
+            searchOrder[_orderID].status = orderState.store_accept;                                      //주문 수락
+        }else{
+            searchOrder[_orderID].status = orderState.store_decline;                                     //주문거절
+            uint refundPrice = searchOrder[_orderID].foodPrice + searchOrder[_orderID].deliveryFee + searchOrder[_orderID].deliveryTip;
+            payable (searchOrder[_orderID].cWallet).transfer(refundPrice);                               //환불 
+        }
+    }
+
+    
+
+
+    //orderState ; 확인
+    function returnOrderState(uint _orderID)public view returns(orderState){
+        return searchOrder[_orderID].status;
+    }
+
+    //가게 음식조리완료
+    function cookFinish(uint _orderID) public {
+        require(msg.sender == searchOrder[_orderID].sWallet);                    //가게 주인확인
+        require(searchOrder[_orderID].status == orderState.store_accept);
+        searchOrder[_orderID].status = orderState.store_cookFinish;
+    }
+
+
+    
+    
+    function getRiderNftTime()public  view returns(uint){
+        return riderContract.getRiderNftTime(msg.sender);
+    }
+    function getStoreNftTime()public  view returns(uint){
+        return Store.getStoreNftTime(msg.sender);
+    }
+    //배달선택(setDelivery)이 안되면, nftMarket가서 burn버튼 클릭하고,nft구매버튼으로 민팅팅
+    function burnRiderNft()public {       
+            riderContract.burn(msg.sender,riderContract.getRiderTokenId(msg.sender));        
+    }
+    function burnStoreNft()public {       
+            Store.storeBurn(msg.sender,Store.getStoreTokenId(msg.sender));        
+    }
+
+    //web3에서 HS-rider컨트랙트의 getRiderNftTime(address)-getBlockTimeStamp() 값을 usestate변수로 받아 남은 일수 표현
+    function getBlockTimeStamp()public view returns(uint){
+        return block.timestamp;
+    }
+
+    //배달원 지정
+    function setDelivery(uint _orderID) public  {
+        require(members[msg.sender].role == Role.rider);                         //배달기사 회원가입여부 확인
+        //이 배달기사가 배달 권한 nft를 가졌는지
+        require(riderContract.getRiderNftTime(msg.sender)>=block.timestamp,"You don't have deliveryNft or Nft is expired");
+        require(searchOrder[_orderID].rWallet == address(0));                    //배달기사 지정 안된 상태인지 확인
+        require(searchOrder[_orderID].status == orderState.store_cookFinish); //가게가 주문 받은 상태인지 확인 
+        searchOrder[_orderID].rWallet = msg.sender;
+        searchOrder[_orderID].status = orderState.rider_accept;
+    }
+
+    
+
+    //배달권 민팅은 원본컨트랙트에서 실행해야하는듯..msg.value가 전달이 안됨..
+    
+    //배달시작
+    function startDelivery(uint _orderID)public{
+        require(searchOrder[_orderID].rWallet == msg.sender);
+        require(searchOrder[_orderID].status == orderState.rider_accept);
+        searchOrder[_orderID].status = orderState.rider_inDelivery;
+    }
+
+    //배달완료
+    function doneDelivery(uint _orderID) public {
+        require(searchOrder[_orderID].rWallet == msg.sender);
+        require(searchOrder[_orderID].status == orderState.rider_inDelivery);    
+        searchOrder[_orderID].status = orderState.rider_deliveryComplete;
+    }
+
+    
+    //거래 완료 후, 가게와 배달원에게 돈 입금
+    function orderComplete(uint _orderID, bool) public {
+        
+        // store Store = new store();                                            //Genesis.sol 파일 인스턴스화
+        uint nftOwner = Store.getMappingAccount(searchOrder[_orderID].sWallet);                            //tokenId가 저장되는 매핑 함수               
+        require(searchOrder[_orderID].cWallet == msg.sender); // 고객이 음식받음 버튼을 누르면 지급됨
+        uint totalFee = (searchOrder[_orderID].deliveryFee *2) + searchOrder[_orderID].deliveryTip;
+        // + 일정시간이 지나거나 owner가 눌러줘도 가능하도록
+        payable(searchOrder[_orderID].rWallet).transfer(totalFee); 
+
+        if(nftOwner > block.timestamp ) { 
+            emit Result("You have an NFT. Platform fees are waived.");
+            payable(searchOrder[_orderID].sWallet).transfer(searchOrder[_orderID].foodPrice); 
+            searchOrder[_orderID].status = orderState.done;
+        } else{ 
+             emit Result("You don't have an NFT or your usage period has expired.  The platform fee is 2% of the foodprice.");
+             uint FoodPrice = searchOrder[_orderID].foodPrice;
+             payable(searchOrder[_orderID].sWallet).transfer((FoodPrice-(FoodPrice * platformFee) /100)); //수수료 부과
+             searchOrder[_orderID].status = orderState.done;
+        }
+    }
+
+    //컨트랙트 돈 인출 
+    function withdraw(uint _money) public onlyOwner{
+        uint possible = address(this).balance -0/*출금불가한 금액*/;
+        require(possible >0);
+        payable(msg.sender).transfer(_money);
+    }
+
+
+    // function returnNftTimeRider()public view returns(uint){
+    //     return riderContract.getRiderNftTime(msg.sender);
+    // }
+    // function returnNftTokenRider()public view returns(uint){
+    //     return riderContract.getRiderTokenId(msg.sender);
+    // }
+    // function returnNftTimeStore()public view returns(uint){
+    //     return Store.getMappingAccount(msg.sender);
+    // }
+}
---
+++ contract/truffle/riderNft.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract delivery is ERC1155Supply,Ownable {
+    
+    uint public cost = 1 ;                         //민팅 비용
+    uint public maxSupply = 10000;                      //nft 공급량
+    uint public nftPerAddressLimit = 1;                 //지갑당 nft 제한
+    mapping(address => uint) public _balances;          //지갑주소당 토큰 아이디 저장
+    mapping(uint => uint) private _mintCounts;          //민트 수
+    using Counters for Counters.Counter;                //유틸 카운터
+    Counters.Counter private _tokenIds; 
+    uint ownerMintedCount;                              //지갑 민팅 횟수
+    uint[] public DeliveryNft;                          //총 공급량 컨트롤하기위해 배열 선언
+    
+    
+    mapping(uint => uint) private _burnTimestamps;   // 민팅 시 _burnTimestamps 매핑에 소각 시간을 저장. 
+    uint private _burnDelay = 60 days;               // 소각 딜레이 
+    
+    mapping(address => uint) public addressMintedBalance; // 민팅횟수를 매핑에 저장.
+  
+    address ownerWallet;
+    constructor() ERC1155 ("https://maroon-mysterious-halibut-244.mypinata.cloud/ipfs/QmbPYnKWCgbGPydScgsnRWCdArPsmewPx59xnd6YiK28q4/riderNft.json") {
+        ownerWallet=msg.sender;
+    }
+      
+
+
+    //라이더가 배달권 가지고 있는지 확인하는require함수(order Contract에서 배달선택할때)
+
+    //nft market의 배달권 구매 버튼 누르면 실행
+    function DeliveryMint(uint amount) public payable {
+          ownerMintedCount = addressMintedBalance[msg.sender];                  //지갑당 민팅횟수를 확인
+          require(DeliveryNft.length < 10000, "The number of members is full"); //총 공급량 확인
+          require(ownerMintedCount < nftPerAddressLimit, "max NFT per address exceeded"); //소지 제한
+          uint tokenId = _tokenIds.current()+1;                                 //토큰의 아이디 카운트
+          require(msg.value >= (cost * amount), "insufficient funds");            //잔액 확인
+          require(DeliveryNft.length + amount < maxSupply, "max NFT limit exceeded");      //민팅 제한
+          _tokenIds.increment();                                                //위 조건 통과시 토큰 아이디 증가
+          _mint(msg.sender, tokenId, amount, "");
+          _burnTimestamps[tokenId] = block.timestamp + _burnDelay;              //민팅한 시간에 유효기간 더하기
+          _balances[msg.sender] = tokenId;
+           DeliveryNft.push(tokenId);
+         for (uint i = 1; i <= amount; i++) {                                   //리콰이어 통과 후 민팅하고나면 addressMintedBalance에 지갑 별로 카운팅이 되며 카운트가 1 이상이면 추가로 민팅이 불가.
+         addressMintedBalance[msg.sender]++;
+         
+         }
+          
+    }
+   
+    //라이더 마이페이지에 번 버튼 필요
+    function burn(address _a,uint tokenId) external {
+        require(_balances[_a] == tokenId, "You are not the Owner");                     // 주인 확인
+        require(_burnTimestamps[tokenId] != 0, "Token does not exist or already burned");       // 생성된 토큰인지 확인
+        require(block.timestamp >= _burnTimestamps[tokenId], "Burn delay has not passed yet");  // 시간 확인
+        _burn(msg.sender, tokenId, balanceOf(_a, tokenId));
+        _burnTimestamps[tokenId] = 0;
+         //리콰이어 통과 후 민팅하고나면 addressMintedBalance에 지갑 별로 카운팅이 되며 카운트가 1 이상이면 추가로 민팅이 불가.
+        addressMintedBalance[_a]--;
+        DeliveryNft.pop();
+    }
+
+    //함수 실행시킨 라이더가 가지고 있는 토큰의 타임스탬프가 몇인지
+    function getRiderNftTime(address _a)external  view returns(uint){
+        return _burnTimestamps[_balances[_a]];
+    }
+    function getRiderTokenId(address _a) external view returns (uint) {
+        return _balances[_a];
+    }
+    
+   
+  
+     //-------------------------------------------------------------테스트----------------------------------------------------//
+     
+        function setBurnDelay(uint tokenId) external onlyOwner {
+        _burnTimestamps[tokenId] = block.timestamp - 70 days;
+    }
+        function getMintCount(uint tokenId) external view returns (uint) {
+        return _mintCounts[tokenId];
+    }
+        function getBurnTimestamp(uint tokenId) external view returns (uint) {
+        return _burnTimestamps[tokenId];
+    }
+        function balanceOf(address account) external view returns (uint) {
+        return _balances[account];
+    }
+        function setBalance(address account, uint balance) external {
+        _balances[account] = balance;
+    }
+        // contract가 _to에게 _amount만큼 보냄
+        function transferTo(address payable _to, uint _amount) public {
+        
+        _to.transfer(_amount);
+    }
+
+    function balan(address) public view returns(uint) {
+       return  _balances[msg.sender];
+    }
+
+    function getLength() public view returns(uint) {
+        return DeliveryNft.length; //배열이름.길이
+    }
+    function getArray() public view returns(uint[] memory) {
+        return DeliveryNft;
+    }
+
+    function withdrawal( uint _amount) public {
+        require(ownerWallet==msg.sender,"you are not owner");
+        payable (ownerWallet).transfer(_amount);
+    }
+
+
+}
+     //-------------------------------------------------------------테스트----------------------------------------------------//
+
+/* 스토어 nft 코드 진행
+     배달원 nft와 거의 흡사한 진행이지만 무료 사용 기간이 있는 nft와 유료로 사용하는 nft를 구분해주어야 하기때문에 storeMint 함수에 if문을 사용해 무료nft,
+     유료nft 민트를 구현함.
+*/
